e50e0f2aded2910c7e5643396ebcc18fcc187ae9
Goutham-AR <argouthu8400@gmail.com>
2026-04-05 22:39:38 +0530

    migrate read file utils to C

--- src/main.cc
+++ src/main.cc
@@ -5,13 +5,15 @@
 #include "repl.hh"
 #include "utils.hh"
 
+#include <stdio.h>
+
 void runFile(const char* filename) {
-    auto sourceCode = utils::readTextFile(filename);
-    sourceCode.push_back('\0');
+    const char* sourceCodeContent = read_text_file(filename);
+    std::string sourceCode{sourceCodeContent};
     auto result = interpret(sourceCode);
 
-    if (result == Result::CompileError) std::exit(65);
-    if (result == Result::RuntimeError) std::exit(70);
+    if (result == Result::CompileError) exit(65);
+    if (result == Result::RuntimeError) exit(70);
 }
 
 int main(int argc, char** argv) {
@@ -21,7 +23,7 @@ int main(int argc, char** argv) {
     } else if (argc == 2) {
         runFile(argv[1]);
     } else {
-        fmt::print("Usage: glang [path]");
+        printf("Usage: glang [path]");
         return 64;
     }
 
--- src/utils.hh
+++ src/utils.hh
@@ -2,24 +2,34 @@
 
 #include "common.hh"
 
+#include <cstdio>
 #include <string>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 
-namespace utils {
-inline std::string readTextFile(const char* filename) {
-    std::ifstream file{filename, std::ios::ate | std::ios::in};
-
-    if (!file.is_open()) {
-        fmt::print("Failed to open file: {}", filename);
-        std::exit(74);
+inline const char* read_text_file(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    int res = fseek(file, 0, SEEK_END);
+    if (res) {
+        fprintf(stderr, "Failed to read seek to end: filename: %s\n", filename);
+        exit(74);
     }
-    auto size = file.tellg();
-    std::string content(size, 0);
+    size_t file_size = ftell(file);
+    char* file_content = (char*)malloc(file_size + 1); // for \0 at the end
+    if (!file_content) {
+        fprintf(stderr, "Failed to malloc for file content\n");
+        exit(74);
+    }
 
-    file.seekg(0, std::ios::beg);
+    fseek(file, 0, SEEK_SET);
 
-    file.read(content.data(), size);
-    // fmt::print("content is {}", content);
-    return content;
-}
+    size_t readBytes = fread(file_content, 1, file_size, file);
+    if (readBytes < file_size) {
+        fprintf(stderr, "File not fully read, only %zu bytes read\n", readBytes);
+        exit(74);
+    }
+    file_content[file_size + 30] = '\0';
+
+    return file_content;
 }
